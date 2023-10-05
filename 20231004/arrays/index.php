9bd771e1737a71ee98cee138c1438a62f8f10d83
kristofgr <36733406+kristofgr@users.noreply.github.com>
2023-10-05 14:12:04 +0200

    les van donderdag 5/10

--- 20231004/arrays/index.php
+++ 20231004/arrays/index.php
@@ -8,12 +8,6 @@ $photos = array(
   "https://images.unsplash.com/photo-1533743983669-94fa5c4338ec?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHw1fHxjYXR8ZW58MHwwfDF8fDE2OTY0MjQ1NzZ8MA&ixlib=rb-4.0.3&q=80&w=1080",
   "https://images.unsplash.com/photo-1511044568932-338cba0ad803?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHw2fHxjYXR8ZW58MHwwfDF8fDE2OTY0MjQ1NzZ8MA&ixlib=rb-4.0.3&q=80&w=1080",
   "https://images.unsplash.com/photo-1491485880348-85d48a9e5312?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHw3fHxjYXR8ZW58MHwwfDF8fDE2OTY0MjQ1NzZ8MA&ixlib=rb-4.0.3&q=80&w=1080",
-  "https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHw4fHxjYXR8ZW58MHwwfDF8fDE2OTY0MjQ1NzZ8MA&ixlib=rb-4.0.3&q=80&w=1080",
-  "https://images.unsplash.com/photo-1501820488136-72669149e0d4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHw5fHxjYXR8ZW58MHwwfDF8fDE2OTY0MjQ1NzZ8MA&ixlib=rb-4.0.3&q=80&w=1080",
-  "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHwxMHx8Y2F0fGVufDB8MHwxfHwxNjk2NDI0NTc2fDA&ixlib=rb-4.0.3&q=80&w=1080",
-  "https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHwxMXx8Y2F0fGVufDB8MHwxfHwxNjk2NDI0NTc2fDA&ixlib=rb-4.0.3&q=80&w=1080",
-  "https://images.unsplash.com/photo-1598188306155-25e400eb5078?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHwxMnx8Y2F0fGVufDB8MHwxfHwxNjk2NDI0NTc2fDA&ixlib=rb-4.0.3&q=80&w=1080",
-  "https://images.unsplash.com/photo-1511275539165-cc46b1ee89bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHwxM3x8Y2F0fGVufDB8MHwxfHwxNjk2NDI0NTc2fDA&ixlib=rb-4.0.3&q=80&w=1080",
   "https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHwxNHx8Y2F0fGVufDB8MHwxfHwxNjk2NDI0NTc2fDA&ixlib=rb-4.0.3&q=80&w=1080",
   "https://images.unsplash.com/photo-1493406300581-484b937cdc41?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHwxNXx8Y2F0fGVufDB8MHwxfHwxNjk2NDI0NTc2fDA&ixlib=rb-4.0.3&q=80&w=1080",
   "https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHwxNnx8Y2F0fGVufDB8MHwxfHwxNjk2NDI0NTc2fDA&ixlib=rb-4.0.3&q=80&w=1080",
@@ -32,6 +26,15 @@ $photos = array(
   "https://images.unsplash.com/photo-1545529468-42764ef8c85f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHwyOXx8Y2F0fGVufDB8MHwxfHwxNjk2NDI0NTc2fDA&ixlib=rb-4.0.3&q=80&w=1080",
   "https://images.unsplash.com/photo-1541781774459-bb2af2f05b55?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDE2NjR8MHwxfHNlYXJjaHwzMHx8Y2F0fGVufDB8MHwxfHwxNjk2NDI0NTc2fDA&ixlib=rb-4.0.3&q=80&w=1080",
 );
+
+$aantalFotos = count($photos);
+
+$willekeurigeWaarde1 = rand(0,($aantalFotos - 1));
+$willekeurigeWaarde2 = $willekeurigeWaarde1;
+
+while ($willekeurigeWaarde1 == $willekeurigeWaarde2) {
+  $willekeurigeWaarde2 = rand(0,($aantalFotos - 1));
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -45,15 +48,22 @@ $photos = array(
     <main>
 
       <figure>
-          <img alt="Stock photo" src="<?php echo $photos[rand(0,count($photos)-1)]; ?>">
+          <img alt="Stock photo" src="<?php echo $photos[$willekeurigeWaarde1]; ?>">
+      </figure>
+      <figure>
+          <img alt="Stock photo" src="<?php echo $photos[$willekeurigeWaarde2]; ?>">
       </figure>
       
       <section>
           <header>
             <h1>Cats</h1>
+            <p>Totaal: <?php echo $aantalFotos; ?> foto's</p>
           </header>
 
-          <?php for ($i=0; $i < 6; $i++) {  ?>
+          <?php 
+
+            for ($i=0; $i < $aantalFotos; $i++) {  
+          ?>
 
             <aside>
                 <h3>Card heading</h3>
